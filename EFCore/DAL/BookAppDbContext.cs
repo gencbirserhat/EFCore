dd422f1e93a191d947ba74842a9c1c0934771b86
gencbirserhat <78283289+gencbirserhat@users.noreply.github.com>
2023-01-16 22:50:55 +0300

    Proje dosyası ekle.

---
+++ EFCore/DAL/BookAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using EFCore.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace EFCore.DAL
+{
+    public class BookAppDbContext: DbContext
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = BookAppDb");
+        }
+    }
+}
---
+++ EFCore/Migrations/20230116194037_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
